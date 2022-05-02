40753db890fd4c704f366f43ad190ddb906d9985
viizx <viceradicaa@gmail.com>
2022-05-02 15:43:59 -0400

    Reservation Schema

---
+++ model/Reservation.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const reservationSchema = new mongoose.Schema(
+  {
+    x: {
+      type: String,
+      required: true,
+      min: 5,
+      max: 255,
+    },
+    fillColor: {
+      type: String,
+      required: true,
+      max: 1024,
+      min: 5,
+    },
+    y: {
+      type: Array,
+      required: true,
+      max: 2,
+      min: 2,
+    },
+  },
+);
+
+module.exports = mongoose.model("Reservation", reservationSchema);
