8defce35efc26673ae3b07a852469ec91c004931
viizx <viceradicaa@gmail.com>
2022-06-17 17:44:18 -0400

    Ship Model

---
+++ model/Ship.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const shipSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  length: {
+    type: Number,
+    required: true,
+  },
+  width: {
+    type: Number,
+    required: true,
+  },
+  capacity: {
+    type: Number,
+    required: true,
+  },
+  crew: {
+    type: Number,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Ship", shipSchema);
