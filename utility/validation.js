5c79d4009ea99f146bc5a6e76dd18345191a0e56
viceradica <viceradicaa@gmail.com>
2022-07-01 15:50:39 +0200

    Added get request by user email

--- utility/validation.js
+++ utility/validation.js
@@ -31,5 +31,67 @@ const loginValidation = (data) => {
   return schema.validate(data)
 }
 
+const shipValidation = (data) => {
+  const schema = joi.object({
+    name: joi.string()
+      .min(3)
+      .required(),
+    length: joi.number()
+      .min(2)
+      .required(),
+    width: joi.number()
+      .min(2)
+      .required(),
+    capacity: joi.number()
+      .min(2)
+      .required(),
+    crew: joi.number()
+      .min(2)
+      .required()
+  })
+
+  return schema.validate(data)
+}
+
+const reservationValidation = (data) => {
+  const schema = joi.object({
+    x: joi.string()
+      .min(3)
+      .required(),
+    y: joi.array()
+      .length(2)
+      .required(),
+    from: joi.string()
+      .min(3)
+      .required(),
+    to: joi.string()
+      .min(3)
+      .required()
+  })
+
+  return schema.validate(data)
+}
+
+const idValidation = (data) => {
+  const schema = joi.string()
+    .length(24)
+    .required()
+
+  return schema.validate(data)
+}
+
+const emailValidation = (data) => {
+  const schema = joi.string()
+    .email()
+    .min(6)
+    .required()
+
+  return schema.validate(data)
+}
+
 module.exports.registerValidation = registerValidation
 module.exports.loginValidation = loginValidation
+module.exports.shipValidation = shipValidation
+module.exports.reservationValidation = reservationValidation
+module.exports.idValidation = idValidation
+module.exports.emailValidation = emailValidation
