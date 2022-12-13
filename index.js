f395c3e7b9f35e6f1b42e5c8ea9c68b92046fbdc
viceradica <vradica_ag@medable.com>
2022-12-13 16:44:23 +0100

    Removing swagger

--- index.js
+++ index.js
@@ -7,9 +7,7 @@ const shipRoute = require('./routes/ships')
 const cors = require('cors')
 const cron = require('node-cron')
 const deleteOld = require('./utility/deleteOld')
-const swaggerUI = require('swagger-ui-express')
-const YAML = require('yamljs')
-const swaggerJsDocs = YAML.load('./openapi.yaml')
+
 dotenv.config()
 
 const app = express()
@@ -31,7 +29,6 @@ app.get('/', (req, res) => {
 app.use('/api/user', authRoute)
 app.use('/api/reservation', reservationRoute)
 app.use('/api/ship', shipRoute)
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerJsDocs))
 
 // Cron Job to delete old reservations every day at midnight
 cron.schedule('0 0 * * *', function() {
